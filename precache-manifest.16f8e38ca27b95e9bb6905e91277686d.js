f9444ed096296c00b2961997b5866836c3d307da
= <=>
2019-07-16 11:47:49 +0200

    Updates

---
+++ precache-manifest.16f8e38ca27b95e9bb6905e91277686d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d512f31824913d3ec5408fa7bf643103",
+    "url": "/github-page-deploy-test/index.html"
+  },
+  {
+    "revision": "caeaf4e219104c1db9a8",
+    "url": "/github-page-deploy-test/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/github-page-deploy-test/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "caeaf4e219104c1db9a8",
+    "url": "/github-page-deploy-test/static/js/main.54c8cc73.chunk.js"
+  },
+  {
+    "revision": "1b0401198de03c1bbacd",
+    "url": "/github-page-deploy-test/static/js/runtime~main.67e5b793.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-page-deploy-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
